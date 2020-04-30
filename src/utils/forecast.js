724536dab978a5e007cddee333d79103ea4aead5
Chris Brock <chrisbrock@Chriss-MBP.localdomain>
2020-04-30 10:10:11 -0500

    Did some cleanup

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            console.log('This is body', JSON.stringify(body));
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + 'You will see a high of ' + body.daily.data[0].temperatureHigh + ', and a low of ' + body.daily.data[0].temperatureLow + '.')
+            callback(undefined,
+                body.daily.data[0].summary + ' It is currently ' +
+                body.currently.temperature + ' degrees out. There is a ' +
+                body.currently.precipProbability + '% chance of rain.' + 'You will see a high of ' +
+                body.daily.data[0].temperatureHigh + ', and a low of ' +
+                body.daily.data[0].temperatureLow + '.')
         }
     })
 }
